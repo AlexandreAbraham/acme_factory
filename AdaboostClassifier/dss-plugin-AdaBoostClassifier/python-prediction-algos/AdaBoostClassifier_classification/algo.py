ef1f0a6a5aa284755c96ef5f6d33d62388b4badf
Marsobad <marine.sobas@dataiku.com>
2021-05-07 10:23:07 +0200

    adaboost classif

---
+++ AdaboostClassifier/dss-plugin-AdaBoostClassifier/python-prediction-algos/AdaBoostClassifier_classification/algo.py
@@ -0,0 +1,12 @@
+from dataiku.doctor.plugins.custom_prediction_algorithm import BaseCustomPredictionAlgorithm
+from sklearn.ensemble import AdaBoostClassifier
+from dku_utils import cast_parameters
+
+class CustomPredictionAlgorithm(BaseCustomPredictionAlgorithm):    
+    def __init__(self, prediction_type=None, params=None):    
+        formatted_parameters = cast_parameters(params)
+        self.clf = AdaBoostClassifier(random_state=formatted_parameters.get('random_state', None))
+        super(CustomPredictionAlgorithm, self).__init__(prediction_type, formatted_parameters)
+    
+    def get_clf(self):
+        return self.clf
